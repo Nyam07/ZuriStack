41a3456129abcfd793bb7e44b352f02504b58715
Nyam07 <bkamunya77@gmail.com>
2022-07-12 14:56:05 +0300

    Create a forms file to specify the user creation and adding parts. Registered the models and specified the outlook of the admin panel in the admin.py file.

--- zuristack_project/apps/accounts/admin.py
+++ zuristack_project/apps/accounts/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from . import models
+from . import forms
+
+@admin.register(models.CustomUser)
+class CustomUserAdmin(UserAdmin):
+    add_form = forms.CustomUserCreationForm
+    form = forms.CustomUserChangeForm
+
+    ordering = ['email']
+
+    list_display =['email', 'is_staff', 'is_active', 'is_superuser']
+    search_fields = ['email']
+
+    add_fieldsets = (
+        (None, {
+            "fields": (
+                'email', 'password'
+            ),
+        }),
+        ('Details', {
+            "fields": (
+                'first_name', 'last_name', 'slug'
+            ),
+        }),
+        ('Permissions', {
+            "fields": (
+                'is_staff', 'is_active'
+            ),
+        }),
+    )
+
+    fieldsets = (
+        (None, {
+            "fields":(
+               'email', 'password' 
+            ),
+        }),
+        ('Details', {
+            "fields":(
+                'first_name', 'last_name', 'slug'
+            ),
+        }),
+        ('Permissions', {
+            "fields":(
+                'is_staff', 'is_active'
+            ),
+        }),
+    )
+
---
+++ zuristack_project/apps/accounts/forms.py
@@ -0,0 +1,13 @@
+from dataclasses import field
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ['email']
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = CustomUser
+        fields =['email']
